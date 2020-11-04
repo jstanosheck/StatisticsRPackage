d794677bcee010da7714108e9c0e25acefa9e55d
John Schwenck <jschwenck@stat.tamu.edu>
2020-11-03 21:27:35 -0600

    deleted R homework functions folder for namespace issues

--- R_homework_functions/LRm_tests.R
+++
@@ -1,9 +0,0 @@
-# LR Multiclass tests
-
-# Load the letter data
-#########################
-letter <- read.table("R_homework_functions/Data/letter-train.txt", header = F, colClasses = "numeric")
-Y <- letter[, 1]
-X <- as.matrix(letter[, -1])
-
-sourceCpp('src/LRMultiClass.cpp')
